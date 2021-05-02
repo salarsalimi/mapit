8b6d68c0ddeee442cab776a259cb05a6b9e63dbf
Salar <salar@localhost.localdomain>
2021-05-02 14:47:19 -0400

    Adding script for linux

---
+++ linux/mapit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+## This program intends to make easy address searching in google map !!
+
+import webbrowser, sys, pyperclip   # you should install pyperclip module by pip
+
+if len(sys.argv) > 1:   # Check to see if we have any input argument
+    address = ' '.join(sys.argv[1:])  # sys.argv=['mapit.py', 'Street', 'ave', ....]
+
+else:
+    address = pyperclip.paste()  # Get address from clipboard
+
+
+
+# https://www.google.com/maps/place/ + address
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
